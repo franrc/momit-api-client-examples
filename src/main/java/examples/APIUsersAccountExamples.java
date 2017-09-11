1530a5abf753766e593965d3da30ffa7e258e258
Daniel Peña Perez <daniel.pena@momit.es>
2017-09-11 12:55:29 +0200

    added user reset and change password examples

--- src/main/java/examples/APIUsersAccountExamples.java
+++ src/main/java/examples/APIUsersAccountExamples.java
@@ -17,6 +17,10 @@ public class APIUsersAccountExamples {
 	 */
 	private static Logger log = Logger.getLogger(APIGeneralExamples.class);
 	public static void main(String[] args) throws URISyntaxException, ClientProtocolException, UnsupportedEncodingException, APIException, IOException{
+		
+		/*******************
+		 * FORGOTTEN PASSWORD
+		 */
 		APIClient api = new APIClient("http://localhost:8080/momitbevel/v2/", "345678");
 		//1. First create a user
 		//1.1 Prepare
@@ -33,6 +37,25 @@ public class APIUsersAccountExamples {
 		api.getSecurityClient().changePassword(user, "9wUDXsf63NCcPghCvIqEC9MvO4I7l0GwcUeRI7WCtGn7mltP1JTHbrdpXKfibpJ1");
 		
 		
+		
+		/*******************
+		 * SIMPLE CHANGE PASSWORD, un usuario logado puede cambiarse su password con el token de session
+		 */
+
+		//1. First create a user
+		//1.1 Prepare
+		user.setEmail("danipenaperez@gmail.com");//random
+		user.setPassword("765123"); 
+		api.connect(user);
+		
+		//2.Forgot password
+		api.getSecurityClient().resetPassword(user); //send email, esperar a que te llegue el mail y coger el token
+		
+		//setear nueva password en el usuario
+		user.setPassword("765123"); 
+		
+		//3. me llega el token de reseteo en el correo, y lo uso asi :
+		api.getSecurityClient().changePassword(user, "9wUDXsf63NCcPghCvIqEC9MvO4I7l0GwcUeRI7WCtGn7mltP1JTHbrdpXKfibpJ1");
 		
 	}
 }
